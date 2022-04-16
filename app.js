8c84e511853a7697f0f798290793daf8f3197ed6
maryh1001 <focusedfitness.mh@gmail.com>
2022-04-16 12:37:47 -0400

    changes

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // make it functional
 
-//
+//make it have question populate when each topic is clicked
+   //Dogs
+   //Sonic
+   //HP
+//when question is answered 
+  //say right or wrong in some way
+  //change correct answer counter to reflect answer
+  //go to next question
+//at the end state score and reset
+  //option to go to main page, redo or choose the other topics
